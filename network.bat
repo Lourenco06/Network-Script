030c950fad4739324c81bac2d2dc9b176081c285
Lourenco06 <125558080+Lourenco06@users.noreply.github.com>
2024-10-17 08:41:56 +0100

    Added comments

---
+++ network.bat
@@ -0,0 +1,5 @@
+REM ********************************************
+REM Programador...: (C) Lourenço Moreira
+REM Data..........: 17/10/24
+REM Descrição.....: Network Script
+REM ********************************************
