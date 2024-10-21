13860624124f5b53545a353f7b4e2404c557062a
Lourenco06 <125558080+Lourenco06@users.noreply.github.com>
2024-10-21 12:40:08 +0100

    Added DNS Query option

--- network.bat
+++ network.bat
@@ -14,15 +14,15 @@ echo ###########################
 echo ============================
 echo 1. IP Configuration
 echo 2. Renew IP Configurations
-echo 3. Shows Network STATS
+echo 3. DNS Lookup
 echo 4. Exit
 echo ============================
 
 set /p option= Select the option and press ENTER: 
 
 if %option% == 1 goto IP
-if %option% == 2 goto RENEW
-if %option% == 3 goto STATS
+if %option% == 2 goto RENEW_IP
+if %option% == 3 goto NS_LOOKUP
 if %option% == 4 goto SAIR
 
 goto MENU
@@ -32,15 +32,17 @@ ipconfig /all
 pause 
 goto MENU
 
-:RENEW
+:RENEW_IP
 ipconfig /release
 pause
 ipconfig /rew
 pause
 goto MENU
 
-:STATS
-netstat
+:NS_LOOKUP
+set /p domain="Insert domain: "
+set /p dnsserver="Insert dnsserver: "
+nslookup %domain% %dnsserver%
 pause
 goto MENU
 
