78f3e4781324a0ff46c8254f8d95c654b9899d3a
Lourenco06 <125558080+Lourenco06@users.noreply.github.com>
2024-10-22 09:31:36 +0100

    Added Text Association

--- network.bat
+++ network.bat
@@ -15,15 +15,19 @@ echo ============================
 echo 1. IP Configuration
 echo 2. Renew IP Configurations
 echo 3. DNS Lookup
-echo 4. Exit
+echo 4. Show MacAdress
+echo 5. Check Energy Efficiency
+echo 6. Exit
 echo ============================
 
 set /p option= Select the option and press ENTER: 
 
 if %option% == 1 goto IP
 if %option% == 2 goto RENEW_IP
-if %option% == 3 goto NS_LOOKUP
-if %option% == 4 goto SAIR
+if %option% == 3 goto DNS_QUERY
+if %option% == 4 goto SHOW_MAC
+if %option% == 5 goto ENERGY_EFFICIENT
+if %option% == 6 goto SAIR
 
 goto MENU
 
@@ -39,10 +43,20 @@ ipconfig /rew
 pause
 goto MENU
 
-:NS_LOOKUP
+:DNS_QUERY
 set /p domain="Insert domain: "
 set /p dnsserver="Insert dnsserver: "
 nslookup %domain% %dnsserver%
+pause
+goto MENU
+
+:SHOW_MAC
+getmac
+pause 
+goto MENU
+
+:ENERGY_EFFICIENT
+powercfg /energy
 pause
 goto MENU
 
