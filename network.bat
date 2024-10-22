6e19deac938d70e7ca532c227d2e812c2ed6ff57
Lourenco06 <125558080+Lourenco06@users.noreply.github.com>
2024-10-22 10:17:34 +0100

    Already added: Show_MAC Energy_Association Disc_Repair QR_Code Leave Option

--- network.bat
+++ network.bat
@@ -17,7 +17,10 @@ echo 2. Renew IP Configurations
 echo 3. DNS Lookup
 echo 4. Show MacAdress
 echo 5. Check Energy Efficiency
-echo 6. Exit
+echo 6. Text Association
+echo 7. Repair Disk
+echo 8. QR code
+echo 9. Exit
 echo ============================
 
 set /p option= Select the option and press ENTER: 
@@ -27,7 +30,10 @@ if %option% == 2 goto RENEW_IP
 if %option% == 3 goto DNS_QUERY
 if %option% == 4 goto SHOW_MAC
 if %option% == 5 goto ENERGY_EFFICIENT
-if %option% == 6 goto SAIR
+if %option% == 6 goto TEXT_ASSOCIATION
+if %option% == 7 goto DISC_REPAIR
+if %option% == 8 goto QR_CODE
+if %option% == 9 goto SAIR
 
 goto MENU
 
@@ -57,6 +63,22 @@ goto MENU
 
 :ENERGY_EFFICIENT
 powercfg /energy
+pause
+goto MENU
+
+:TEXT_ASSOCIATION
+assoc >> OUTPUT.txt
+pause
+goto MENU
+
+:DISC_REPAIR
+chkdsk /r
+pause 
+goto MENU
+
+:QR_CODE
+set /p url="Insert url: "
+ping %url%
 pause
 goto MENU
 
